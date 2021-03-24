eb423e16866647725755238ab2f2b4a396c264e2
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-03-24 16:00:38 +0100

    pretty solution implemented

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,33 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+
+        List<String> result = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            TreeMap<Integer, String> competitors = new TreeMap<>(Collections.reverseOrder());
+
+            while (scanner.hasNext()) {
+                Scanner line = new Scanner(scanner.nextLine());
+                line.useDelimiter(",");
+                String name = line.next();
+                Integer points = (line.nextInt() + line.nextInt() + line.nextInt() + line.nextInt() + line.nextInt());
+                competitors.put(points, name);
+            }
+
+            result = new ArrayList<>(competitors.values());
+
+            return result;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return result;
+        }
     }
 
 }
