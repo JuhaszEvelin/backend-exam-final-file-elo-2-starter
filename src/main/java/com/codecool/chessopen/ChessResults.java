db6918d0524b21b082a281d0e54d7eae770105b1
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-03-24 16:09:06 +0100

    refactored

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -7,27 +7,30 @@ public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName) {
 
-        List<String> result = new ArrayList<>();
+        TreeMap<Integer, String> competitors = getCompetitorsSortedByPoints(fileName);
+
+        return new ArrayList<>(competitors.values());
+    }
+
+    private TreeMap<Integer, String> getCompetitorsSortedByPoints(String fileName) {
+
+        TreeMap<Integer, String> competitors = new TreeMap<>(Collections.reverseOrder());
 
         try (Scanner scanner = new Scanner(new File(fileName))) {
-            TreeMap<Integer, String> competitors = new TreeMap<>(Collections.reverseOrder());
 
             while (scanner.hasNext()) {
                 Scanner line = new Scanner(scanner.nextLine());
                 line.useDelimiter(",");
-                String name = line.next();
                 Integer points = (line.nextInt() + line.nextInt() + line.nextInt() + line.nextInt() + line.nextInt());
+                String name = line.next();
                 competitors.put(points, name);
             }
 
-            result = new ArrayList<>(competitors.values());
-
-            return result;
-
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
-            return result;
         }
+
+        return competitors;
     }
 
 }
